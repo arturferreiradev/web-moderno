3af28c2e57f822f0c854eac22fbadd1af54dcde0
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-12 03:55:54 -0300

    Cuidados com Number em JS

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,6 +1,7 @@
 // Exemplos de comportamento no JS 
 console.log(7 / 0);
 console.log("10" / 2);
+console.log("3" + 2); // String ganha!
 console.log("Show!" * 2);
 console.log(0.1 + 0.7);
 // console.log(10.toString());
