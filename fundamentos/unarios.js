2284244144397708b201d382cfa9a9d78c03baa7
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-12 19:06:52 -0300

    Operadores: Unários

---
+++ fundamentos/unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1;
+let num2 = 2;
+
+num1++;
+console.log(num1);
+--num1;
+console.log(num1);
+
+console.log(++num1 == num2--);
+console.log(++num1 == num2--); // não faça isso, faça códigos simples de serem lidos, lembre-se: explícito é melhor que implícito
