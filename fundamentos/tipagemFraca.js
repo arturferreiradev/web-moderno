4b35df91cf5151598cbadce918d872f0b4544119
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-12 03:18:00 -0300

    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+// Tipagem fraca: permite atribuir tipos valores diferentes do inicialmente atribuido
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nome genéricos e siglas:
+let valor = '';
+let numero = 1;
+let pqp = false; // Produto Químico Perigoso... kkkkk
