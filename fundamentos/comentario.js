8526acf6f470dbec76f6e4c6f14652f84d18d54a
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-12 03:02:26 -0300

    Comentários de código

---
+++ fundamentos/comentario.js
@@ -0,0 +1,18 @@
+// Comentário de uma linha
+console.log('Linha 1');
+
+/*
+    Comentário de 
+    múltiplas linhas
+*/
+
+console.log('Linha 2');
+
+/*
+    * Comentário de
+    * múltiplas linhas
+    * formatado diferente.
+*/
+console.log('Linha 3')
+
+// Use os comentários com sabedoria
