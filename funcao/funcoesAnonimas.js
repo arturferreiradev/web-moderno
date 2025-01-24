07f288cff447424a2ea413f164986bbd55d0b12c
Artur Ferreira <ferreira.artur.dev@gmail.com>
2025-01-23 21:12:11 -0300

    Funções Anônimas

--- funcao/funcoesAnonimas.js
+++ funcao/funcoesAnonimas.js
@@ -3,7 +3,7 @@ const soma = function (x, y) {
 }
 
 const imprimirResultado = function (a, b, operacao = soma) {
-    console.log(operacao(a, b))
+    console.log(operacao(a, b));
 }
 
 imprimirResultado(3, 4);
